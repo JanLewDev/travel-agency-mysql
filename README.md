a114ed11469b71d1e2d4e585a3bcf6082f2e6433
Jan Lewandowski <lewandowski.jan@o2.pl>
2024-11-16 16:09:33 +0100

    feat: plik testowy, przyklad .env

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Projekt z baz danych
 
 Autorzy:
+
 - Cezary Czetyrba
 - Jan Lewandowski
 - Michał Mickiewicz
 - Szymon Jończak-Lis
-
---
+++ test.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cnx = mysql.connector.connect(
+    host="giniewicz.it",
+    port=3306,
+    user="student",
+    password=os.getenv("CONNECTION_PASSWORD"),
+    database="sakila",
+)
+
+cursor = cnx.cursor()
+cursor.execute("SELECT * FROM actor")
+for row in cursor:
+    print(row)
+
+cursor.close()
+cnx.close()
